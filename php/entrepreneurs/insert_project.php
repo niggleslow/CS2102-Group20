2aa63c6c791dfc4299e69742c42ba049176c490f
kester <straw000@gmail.com>
2016-04-10 23:25:37 +0800

    Fixed bug

--- php/entrepreneurs/insert_project.php
+++ php/entrepreneurs/insert_project.php
@@ -21,16 +21,17 @@ $description = $_POST["description"];
 $start_date = $_POST["start_date"];
 $duration = int($_POST["duration"]);
 $category = $_POST["category"];
-$amount = $_POST["amount"];
+$amount = int($_POST["amount"]);
+$remaining_amount = int($_POST["remaining_amount"]);
 
 $username = $_SESSION["username"];
 
 // select a row from the database for login and check that array is not empty
-$query = "INSERT INTO projects(title, description, start_date, duration, category, remaining_amount, e_name, amount) VALUES ('$title', '$description', '$start_date', $duration, '$category', int($amount), '$username', int($amount))";
+$query = "INSERT INTO projects(title, description, start_date, duration, category, remaining_amount, e_name, amount) VALUES ('$title', '$description', '$start_date', $duration, '$category', $remaining_amount, '$username', $amount)";
 $result = pg_query($query) or die("Query failed: " . pg_last_error());
 
 // select a row from the database for login and check that array is not empty
-$query = "INSERT INTO create_project(entrepreneur_name, project_title) VALUES ('$username', $title)";
+$query = "INSERT INTO create_project(entrepreneur_name, project_title) VALUES ('$username', '$title')";
 $result = pg_query($query) or die("Query failed: " . pg_last_error());
 
 ?>
